c3858fa8d1e54f08601cb2c2bcc496dab6353939
genaroibc <bonavitaismael@gmail.com>
2023-05-05 10:46:04 -0300

    feat: add widget as an iframe

---
+++ src/components/IframeWidget.tsx
@@ -0,0 +1,10 @@
+export function IframeWidget() {
+  return (
+    <iframe
+      title="Squid Widget"
+      width="420"
+      height="641"
+      src="https://widget.squidrouter.com/iframe?config=%7B%22companyName%22%3A%22Custom%22%2C%22style%22%3A%7B%22neutralContent%22%3A%22%23B1ACA1%22%2C%22baseContent%22%3A%22%23E7D3AA%22%2C%22base100%22%3A%22%23474745%22%2C%22base200%22%3A%22%23212428%22%2C%22base300%22%3A%22%2313181D%22%2C%22error%22%3A%22%23ED6A5E%22%2C%22warning%22%3A%22%23FFB155%22%2C%22success%22%3A%22%2362C555%22%2C%22primary%22%3A%22%23E0C072%22%2C%22secondary%22%3A%22%23E9DCBD%22%2C%22secondaryContent%22%3A%22%2301020C%22%2C%22neutral%22%3A%22%230C0F12%22%2C%22roundedBtn%22%3A%221rem%22%2C%22roundedBox%22%3A%221rem%22%2C%22roundedDropDown%22%3A%221rem%22%2C%22displayDivider%22%3Afalse%7D%2C%22slippage%22%3A1.5%2C%22infiniteApproval%22%3Afalse%2C%22enableExpress%22%3Afalse%2C%22apiUrl%22%3A%22https%3A%2F%2Fapi.squidrouter.com%22%2C%22mainLogoUrl%22%3A%22%22%2C%22comingSoonChainIds%22%3A%5B%22cosmoshub-4%22%2C%22injective-1%22%2C%22axelar-dojo-1%22%2C%22kichain-2%22%5D%2C%22titles%22%3A%7B%22swap%22%3A%22Convert%22%2C%22settings%22%3A%22Settings%22%2C%22wallets%22%3A%22Wallets%22%2C%22tokens%22%3A%22Tokens%22%2C%22chains%22%3A%22Chains%22%2C%22history%22%3A%22History%22%2C%22transaction%22%3A%22Transaction%22%2C%22allTokens%22%3A%22Tokens%22%2C%22destination%22%3A%22Destination%20address%22%7D%2C%22priceImpactWarnings%22%3A%7B%22warning%22%3A3%2C%22critical%22%3A5%7D%7D"
+    ></iframe>
+  )
+}
