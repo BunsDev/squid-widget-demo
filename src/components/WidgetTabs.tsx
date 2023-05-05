b013cdc579dba01bb8230247d129a426225dce61
genaroibc <bonavitaismael@gmail.com>
2023-05-05 13:44:07 -0300

    feat: add support for widget config when using it as an iframe

--- src/components/WidgetTabs.tsx
+++ src/components/WidgetTabs.tsx
@@ -9,86 +9,76 @@ export function WidgetTabs() {
   const [visibleWidget, setVisibleWidget] = useState(WIDGETS_LIST[0])
   const [widgetTheme, setWidgetTheme] = useState(WIDGET_THEMES[0])
   const [areCustomStylesEnabled, setAreCustomStylesEnabled] = useState(false)
-  const [isIframeWidgetVisible, setIsIframeWidgetVisible] = useState(false)
+  const [isRenderAsIframeEnabled, setIsRenderAsIframeEnabled] = useState(false)
 
   return (
     <section
       className={areCustomStylesEnabled ? "widget-custom-styles-enabled" : ""}
     >
       <nav className="flex flex-wrap flex-row items-center justify-center gap-4 p-4">
-        {WIDGETS_LIST.map(widget => (
+        {WIDGETS_LIST.map((widget) => (
           <WidgetTabControl
             key={widget.id}
-            visible={visibleWidget.id === widget.id && !isIframeWidgetVisible}
-            handleClick={() => {
-              setVisibleWidget(widget)
-              setIsIframeWidgetVisible(false)
-            }}
+            visible={visibleWidget.id === widget.id}
+            handleClick={() => setVisibleWidget(widget)}
           >
             {widget.name}
           </WidgetTabControl>
         ))}
-
-        <WidgetTabControl
-          visible={isIframeWidgetVisible}
-          handleClick={() => setIsIframeWidgetVisible(true)}
-        >
-          Iframe
-        </WidgetTabControl>
       </nav>
 
       <article className="w-full grid items-start grid-cols-1 md:grid-cols-2 gap-6 p-4 mx-auto max-w-5xl">
         <div className="flex justify-center items-center min-h-[640px]">
-          {isIframeWidgetVisible ? (
-            <IframeWidget />
+          {isRenderAsIframeEnabled ? (
+            <IframeWidget
+              config={{
+                ...visibleWidget.config,
+                style: widgetTheme.style,
+              }}
+            />
           ) : (
             <SquidWidget
               config={{
                 ...visibleWidget.config,
-                style: widgetTheme.style
+                style: widgetTheme.style,
               }}
             />
           )}
         </div>
 
         <div className="flex flex-col items-center gap-4">
-          {isIframeWidgetVisible ? (
-            <p className="text-center bg-slate-700 rounded-md py-2 px-4">
-              The Squid Widget using the iframe
-            </p>
-          ) : (
-            <>
-              <p className="text-center bg-slate-700 rounded-md py-2 px-4">
-                {visibleWidget.description}
-              </p>
+          <p className="text-center bg-slate-700 rounded-md py-2 px-4">
+            {visibleWidget.description}
+          </p>
 
-              <code className="w-full flex-1 max-w-lg overflow-auto">
-                <pre>
-                  config: {JSON.stringify(visibleWidget.config, null, 2)}
-                </pre>
-              </code>
+          <code className="w-full flex-1 max-w-lg overflow-auto">
+            <pre>config: {JSON.stringify(visibleWidget.config, null, 2)}</pre>
+          </code>
 
-              <nav className="flex flex-wrap gap-4 items-center justify-center">
-                {WIDGET_THEMES.map(theme => (
-                  <button
-                    key={theme.name}
-                    className="bg-slate-950 hover:bg-slate-900 border-2 border-transparent transition-colors hover:border-gray-300 rounded-full py-2 px-4 flex items-center gap-2"
-                    onClick={() => setWidgetTheme(theme)}
-                  >
-                    <span
-                      className={`inline-block w-4 h-4 rounded-full ${theme.color}`}
-                    ></span>
-                    {theme.name}
-                  </button>
-                ))}
+          <nav className="flex flex-wrap gap-4 items-center justify-center">
+            {WIDGET_THEMES.map((theme) => (
+              <button
+                key={theme.name}
+                className="bg-slate-950 hover:bg-slate-900 border-2 border-transparent transition-colors hover:border-gray-300 rounded-full py-2 px-4 flex items-center gap-2"
+                onClick={() => setWidgetTheme(theme)}
+              >
+                <span
+                  className={`inline-block w-4 h-4 rounded-full ${theme.color}`}
+                ></span>
+                {theme.name}
+              </button>
+            ))}
 
-                <ToggleBar
-                  label="Enable direct CSS styling"
-                  onToggle={checked => setAreCustomStylesEnabled(checked)}
-                />
-              </nav>
-            </>
-          )}
+            <ToggleBar
+              label="Enable direct CSS styling"
+              onToggle={(checked) => setAreCustomStylesEnabled(checked)}
+              disabled={isRenderAsIframeEnabled}
+            />
+            <ToggleBar
+              label="Render as Iframe"
+              onToggle={(checked) => setIsRenderAsIframeEnabled(checked)}
+            />
+          </nav>
         </div>
       </article>
     </section>
